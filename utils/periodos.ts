1e00f802e4887d0a886225d44c4d9941da9e20a7
Camilo Finova <juancamilog9911@gmail.com>
2026-01-08 19:36:29 +0000

    ♻️ refactor: ajusta la función `getAvailablePeriodos` para considerar el año actual
    Se modifica la función `getAvailablePeriodos` en `periodos.ts` para que devuelva un array de periodos disponibles basado en el año actual y el estado `periodo` del store. Se implementa lógica adicional para manejar casos donde el periodo es mayor o igual al año actual, mejorando la flexibilidad de la función.

--- utils/periodos.ts
+++ utils/periodos.ts
@@ -1,7 +1,13 @@
+"use client"
 import { usePeriodoStore } from "@/store/periodo.store"
-const { periodo } = usePeriodoStore()
-
-
 export const getAvailablePeriodos = () => {
-  return Array.from({ length: 10 }, (_, i) => periodo + i).sort((a: number, b: number) => b - a)
-} 
+  const { periodo } = usePeriodoStore()
+  const currentYearNum = new Date().getFullYear()
+
+  if (periodo === currentYearNum) {
+    return Array.from({ length: 5 }, (_, i) => periodo + i).sort((a: number, b: number) => b - a)
+
+  } else if (periodo > currentYearNum) {
+    return Array.from({ length: 5 }, (_, i) => currentYearNum + i).sort((a: number, b: number) => b - a)
+  }
+}
