11291bb2f5bc9c009ca88dcd9bce718b61c28e79
Camilo Finova <juancamilog9911@gmail.com>
2026-01-14 21:13:42 +0000

    ♻️ refactor: actualiza la función getAvailablePeriodos para aceptar un parámetro de periodo
    Se modifica la función getAvailablePeriodos para que reciba un parámetro de periodo, mejorando la flexibilidad en la obtención de periodos disponibles. Se optimiza la lógica de retorno para manejar correctamente los casos de periodo actual y futuro, asegurando un manejo más claro de los datos.

--- utils/periodos.ts
+++ utils/periodos.ts
@@ -1,13 +1,15 @@
-"use client"
-import { usePeriodoStore } from "@/store/periodo.store"
-export const getAvailablePeriodos = () => {
-  const { periodo } = usePeriodoStore()
+export const getAvailablePeriodos = (periodo: number) => {
   const currentYearNum = new Date().getFullYear()
 
   if (periodo === currentYearNum) {
-    return Array.from({ length: 5 }, (_, i) => periodo + i).sort((a: number, b: number) => b - a)
-
-  } else if (periodo > currentYearNum) {
-    return Array.from({ length: 5 }, (_, i) => currentYearNum + i).sort((a: number, b: number) => b - a)
+    return Array.from({ length: 5 }, (_, i) => periodo + i)
+      .sort((a, b) => b - a)
   }
+
+  if (periodo > currentYearNum) {
+    return Array.from({ length: 5 }, (_, i) => currentYearNum + i)
+      .sort((a, b) => b - a)
+  }
+
+  return []
 }
